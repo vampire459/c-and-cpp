96f0a50a975f93717bcbc7b3b97e6ea704687ada
Aryan Singh <iamabloody@gmail.com>
2021-05-10 18:02:40 +0530

    🚀

--- greet.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(void)
-{
-    std::string fName;
-    std::cout << "What's your first name?" << std::endl;
-    std::cin >> fName;
-    std::cout << "Hello "<< fName << std::endl;
-}
