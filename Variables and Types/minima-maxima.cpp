ad704e265d241387daeb47c792d06072345383ba
Aryan Singh <iamabloody@gmail.com>
2021-05-11 14:38:56 +0530

    Size of int

---
+++ Variables and Types/minima-maxima.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int Num=9 ;
+
+	cout << "Size OF int is: " << sizeof(Num) << "bytes" << endl;
+	cout << "Size of int (min): " << INT8_MIN << endl;
+	cout << "Size of int (max): " << INT8_MAX << endl;
+
+	return 0;
+}
+
