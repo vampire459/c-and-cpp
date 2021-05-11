64d4518119ae76ddcd2c2bc9bf2b42384a18b5bd
Aryan Singh <iamabloody@gmail.com>
2021-05-11 16:41:43 +0530

    data type done.

--- Variables and Types/minima-maxima.cpp
+++ Variables and Types/minima-maxima.cpp
@@ -4,12 +4,21 @@ using namespace std;
 
 int main(void)
 {
-	int Num=9 ;
-
-	cout << "Size OF int is: " << sizeof(Num) << "bytes" << endl;
+	int Num=9, Num1 ;
+	char character= 'a';
+	bool boolean = true;
+	float pie = 3.14;
+	double poojaBankAccount =516461616; // That's just my sister, she hasn't withdrawn her savings account in 3 months
+	
+	cout << "Size is:  " << sizeof(Num) << "bytes" << endl;
+	cout << "Size is:  " << sizeof(char) << "bytes" << endl;
+	cout << "Size is:  " << sizeof(bool) << "bytes" << endl;
+	cout << "Size is:  " << sizeof(float) << "bytes" << endl;
+	cout << "Size is:  " << sizeof(double) << "bytes" << endl;
 	cout << "Size of int (min): " << INT8_MIN << endl;
 	cout << "Size of int (max): " << INT8_MAX << endl;
-
+	cout << "Size of the unsigned int is: " << sizeof(unsigned int) << " bytes" << endl;
+	cout << "Size of the UINT is: " << UINT8_MAX << " bytes" << endl;
 	return 0;
 }
 
