cdc96098dd977c12319178180c751db1ea6f39d9
Aryan Singh <iamabloody@gmail.com>
2021-05-11 18:35:05 +0530

    ascii refined

--- Variables and Types/asciitable.cpp
+++ Variables and Types/asciitable.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-
+using namespace std;
 
 int main(void)
 {
     char character; 
-    std::cout << "Enter any character:";
-    std::cin >> character ;
-    std::cout << "The ascii equivalent for the entered character is: " << int(character) << std::endl; 
-
+        std::cout << "Enter any character:";
+        std::cin >> character ;
+        std::cout << "The ascii equivalent for the entered character is: " << int(character) << std::endl; 
+    return 0;
 }
+
+
