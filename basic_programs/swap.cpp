f9c789073f0b6adec0159984fe731849cf08cc49
Aryan Singh <iamabloody@gmail.com>
2021-05-16 21:07:27 +0530

    swap

---
+++ basic_programs/swap.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string a = "value1";
+	string b = "value2";
+	cout << "Value of a before: " << a << endl;
+	cout << "Value of b before: " << b << endl;
+	swap(a, b);
+	cout << "Value of a now: " << a << endl;
+	cout << "Value of b now: " << b << endl;
+	return 0;
+}
