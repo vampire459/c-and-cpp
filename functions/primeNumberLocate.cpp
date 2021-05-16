a2c3262295e1d874305ed3c796b5aa847e7c4269
Aryan Singh <iamabloody@gmail.com>
2021-05-16 19:51:56 +0530

    prime number

---
+++ functions/primeNumberLocate.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void primeNum(int number);
+
+int main()
+{
+    int num;
+
+    cout  << "enter number: ";
+    cin >> num;
+
+    primeNum(num);
+}
+
+void primeNum(int number)
+{
+    cout << "Is it a prime number?" << endl;
+
+    bool isPrimeNumber = true;
+
+    for (int i = 2; i < number; i++)
+    {
+        if (number%i == 0)
+        {
+            isPrimeNumber = false;
+            break;
+        }
+    }
+
+    if(isPrimeNumber)
+        cout << "yes prime number\n"; 
+    else
+        cout << "not prime number\n"; 
+}
